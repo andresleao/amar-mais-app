9e2a2849b2e616aa97224b713b9ae03fbf229630
André Leão <andrefelipesl123@gmail.com>
2021-10-03 10:21:43 -0300

    implementado item findById

--- src/main/java/com/acc/amar/mais/controllers/ItemController.java
+++ src/main/java/com/acc/amar/mais/controllers/ItemController.java
@@ -1,20 +1,25 @@
 package com.acc.amar.mais.controllers;
 
-import com.acc.amar.mais.dtos.ItemDto;
-import com.acc.amar.mais.mapper.ItemMapper;
-import com.acc.amar.mais.models.Item;
-import com.acc.amar.mais.services.ItemService;
+import java.net.URI;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import javax.validation.Valid;
-import java.net.URI;
+import com.acc.amar.mais.dtos.ItemDto;
+import com.acc.amar.mais.dtos.ItemNewDTO;
+import com.acc.amar.mais.mapper.ItemMapper;
+import com.acc.amar.mais.models.Item;
+import com.acc.amar.mais.services.ItemService;
 
 @CrossOrigin("*")
 @RestController
@@ -33,4 +38,19 @@ public class ItemController {
                 .toUri();
         return ResponseEntity.created(uri).build();
     }
+    
+    @GetMapping(value = "/{id}")
+	public ResponseEntity<ItemNewDTO> findById(@PathVariable Integer id) {
+		Item item = service.findById(id);
+		
+		ItemNewDTO dto = new ItemNewDTO();
+		dto.setId(item.getId());
+		dto.setClassificacao(item.getClassificacao());
+		dto.setFoto(dto.getFoto());
+		dto.setNome(item.getNome());
+		
+		return ResponseEntity.ok().body(dto);
+	}
+    
+    
 }
--- src/main/java/com/acc/amar/mais/services/ItemService.java
+++ src/main/java/com/acc/amar/mais/services/ItemService.java
@@ -1,9 +1,13 @@
 package com.acc.amar.mais.services;
 
-import com.acc.amar.mais.models.Item;
-import com.acc.amar.mais.repositories.ItemRepository;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.acc.amar.mais.models.Item;
+import com.acc.amar.mais.repositories.ItemRepository;
+import com.acc.amar.mais.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class ItemService {
@@ -15,4 +19,10 @@ public class ItemService {
         Item itemCriado = repository.save(item);
         return itemCriado;
     }
+    
+    public Item findById(Integer id) {
+		Optional<Item> item = repository.findById(id);
+		return item.orElseThrow(
+				() -> new ObjectNotFoundException("Item não encontrado! Id: " + id));
+	}
 }
